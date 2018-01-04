79c1ad56d586e626ba094119771103befd5b4796
Samuel Lo <locheukyee2001@gmail.com>
2018-01-05 03:58:39 +0800

    update

---
+++ DoNotStarveTogether/Cluster_3/Master/modoverrides.lua
@@ -0,0 +1,25 @@
+return {
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  } 
+}
